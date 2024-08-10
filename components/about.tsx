f613d200d6a7f589cbd1d5bef9aeb75580f9e8de
Galah-Studio <hamid@galah.tv>
2024-08-10 22:13:53 +0300

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -3,9 +3,8 @@
 
 // Import necessary dependencies and components.
 import { motion } from "framer-motion";
-
+import { EXTRA_LINKS, OWNER_NAME } from "@/constants"; // Import OWNER_NAME
 import { useSectionInView } from "@/lib/hooks";
-
 import SectionHeading from "./section-heading";
 
 // Define the About component.
@@ -29,19 +28,27 @@ const About = () => {
       {/* Display a paragraph with information about Galah Studio's background and focus. */}
       <p className="mb-3">
         <span
-            className="text-[#ff2b69] glow-effect"
-            style={{
-              textShadow: "0 0 120px #ff2b69, 0 0 40px #ff2b69",
-            }}
-          >
-            {OWNER_NAME.split(" ")[0]}
-          </span>
-         Studio is dedicated to crafting exquisite <b className="font-medium">animation shows and commercials</b>. Our team of experienced filmmakers and animators brings stories to life with creativity and passion. We also specialize in producing high-quality <b className="font-medium">live-action commercials</b>, ensuring top-notch visual storytelling in every project.
+          className="text-[#ff2b69] glow-effect"
+          style={{
+            textShadow: "0 0 8px #ff2b69, 0 0 10px #ff2b69, 0 0 12px #ff2b69",
+          }}
+        >
+          {OWNER_NAME.split(" ")[0]}
+        </span>{" "}
+        Studio is dedicated to crafting exquisite{" "}
+        <b className="font-medium">animation shows and commercials</b>. Our
+        team of experienced filmmakers and animators brings stories to life with
+        creativity and passion. We also specialize in producing high-quality{" "}
+        <b className="font-medium">live-action commercials</b>, ensuring
+        top-notch visual storytelling in every project.
       </p>
 
       {/* Display another paragraph about the studio's philosophy and approach. */}
       <p>
-        Our focus is on delivering visually stunning and emotionally engaging content. We believe in the power of storytelling and strive to create works that resonate with audiences. Whether it's an animation show or a live-action commercial, our goal is to captivate and inspire.
+        Our focus is on delivering visually stunning and emotionally engaging
+        content. We believe in the power of storytelling and strive to create
+        works that resonate with audiences. Whether it's an animation show or a
+        live-action commercial, our goal is to captivate and inspire.
       </p>
     </motion.section>
   );
