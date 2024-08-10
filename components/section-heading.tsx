a23aaf9a00898a1359b5c90c907bb71c49f98178
Galah-Studio <hamid@galah.tv>
2024-08-10 23:10:06 +0300

    Update section-heading.tsx

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -3,12 +3,13 @@ import React from "react";
 // Define the prop type for the SectionHeading component.
 type SectionHeadingProps = {
   children: React.ReactNode; // Children are of type React.ReactNode.
+  className?: string; // Optional className prop to allow custom styling.
 };
 
 // SectionHeading component takes the children prop and displays it as an h2 element.
-const SectionHeading = ({ children }: SectionHeadingProps) => {
+const SectionHeading = ({ children, className }: SectionHeadingProps) => {
   return (
-    <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+    <h2 className={`text-3xl font-medium capitalize mb-8 text-center ${className}`}>
       {children}
     </h2>
   );
