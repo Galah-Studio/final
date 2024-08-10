f306618cc63aed639891ae54291106b5cd9620cb
Galah-Studio <hamid@galah.tv>
2024-08-10 16:50:57 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
       >
         {/* bg blur effect */}
         <div className="bg-[#fbe2e3] dark:bg-[#632236] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
-        <div className="bg-[#dbd7fb] dark:bg-[##290E16] absolute top-[-1rem] -z-10 left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
+        <div className="bg-[#dbd7fb] dark:bg-[#290E16] absolute top-[-1rem] -z-10 left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
 
         {/* theme context provider */}
         <ThemeContextProvider>
