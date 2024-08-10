aed7d7ee261fe4642c34f4b5c1a6d48386b4f106
Galah-Studio <hamid@galah.tv>
2024-08-10 16:00:59 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,8 +68,8 @@ export default function RootLayout({
         className={`${inter.className} bg-gray-50 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 text-gray-950 relative pt-28 sm:pt-36`}
       >
         {/* bg blur effect */}
-        <div className="bg-[#fbe2e3] dark:bg-[#522f42] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
-        <div className="bg-[#dbd7fb] dark:bg-[#7d5a70] absolute top-[-1rem] -z-10 left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
+        <div className="bg-[#fbe2e3] dark:bg-[#632236] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
+        <div className="bg-[#dbd7fb] dark:bg-[#632236] absolute top-[-1rem] -z-10 left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
 
         {/* theme context provider */}
         <ThemeContextProvider>
