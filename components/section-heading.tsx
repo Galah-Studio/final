de4a2cd915f4e9850f52b00f4d59dd128f1dc7f8
Galah-Studio <hamid@galah.tv>
2024-08-10 23:12:43 +0300

    Update section-heading.tsx

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -7,7 +7,7 @@ type SectionHeadingProps = {
 };
 
 // SectionHeading component takes the children prop and displays it as an h2 element.
-const SectionHeading = ({ children, className }: SectionHeadingProps) => {
+const SectionHeading = ({ children, className = "" }: SectionHeadingProps) => {
   return (
     <h2 className={`text-3xl font-medium capitalize mb-8 text-center ${className}`}>
       {children}
