488b0d4243167ac9a8ba0cd841729126a17bc03d
Galah-Studio <hamid@galah.tv>
2024-08-10 21:09:48 +0300

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -19,7 +19,7 @@ const Header = () => {
       <motion.div
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-pink-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-pink-990 dark:border-black/40 dark:bg-opacity-75"
       />
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
@@ -57,7 +57,7 @@ const Header = () => {
                       stiffness: 300,
                       damping: 30,
                     }}
-                    className="bg-gray-200/50 rounded-full absolute inset-0 -z-10 dark:bg-pink-300"
+                    className="bg-gray-200/50 rounded-full absolute inset-0 -z-10 dark:bg-pink-550"
                   />
                 )}
               </Link>
