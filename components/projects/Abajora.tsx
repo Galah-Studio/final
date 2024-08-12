57a3ade27f4b384f54b03582ad7b09528370323c
Galah-Studio <hamid@galah.tv>
2024-08-13 02:35:50 +0300

    Create Abajora.tsx

---
+++ components/projects/Abajora.tsx
@@ -0,0 +1,21 @@
+import ProjectHeader from './ProjectHeader';
+import SpotLight from './SpotLight';
+import Logos from './Logos';
+import AboutSection from './AboutSection';
+import ProcessSection from './ProcessSection';
+import SkillsSection from './SkillsSection';
+
+const Abajora = () => {
+    return (
+        <div>
+            <ProjectHeader />
+            <SpotLight />
+            <Logos />
+            <AboutSection />
+            <ProcessSection />
+            <SkillsSection />
+        </div>
+    );
+};
+
+export default Abajora;
