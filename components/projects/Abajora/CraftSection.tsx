beeb83dd667bb3916f03639e988d062d8707856d
Galah-Studio <hamid@galah.tv>
2024-08-13 05:01:57 +0300

    Update CraftSection.tsx

--- components/projects/Abajora/CraftSection.tsx
+++ components/projects/Abajora/CraftSection.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SectionHeading from "@/components/SectionHeading"; // Adjust the path based on your folder structure
 
 const CraftSection = () => {
   return (
-    <section className="py-16 px-6 bg-gray-100 text-black">
-      <h2 className="text-3xl font-bold mb-6">Process</h2>
+    <section className="py-16 px-6 bg-gray-100 text-black" id="craft">
+      <SectionHeading>Process</SectionHeading>
       <div className="space-y-8">
         <div className="flex flex-col md:flex-row">
           <img src="/path-to-image.jpg" alt="Process Image" className="w-full md:w-1/2 mb-4 md:mb-0 md:mr-4" />
