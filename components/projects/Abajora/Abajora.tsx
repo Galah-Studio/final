8da03f329727a19f3464a28d8a965718733fd785
Galah-Studio <hamid@galah.tv>
2024-08-13 05:03:44 +0300

    Update Abajora.tsx

--- components/projects/Abajora/Abajora.tsx
+++ components/projects/Abajora/Abajora.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ProjectHeader from './ProjectHeader';
 import SpotLight from './SpotLight';
 import StorySection from './StorySection';
@@ -6,10 +7,9 @@ import ToolsSection from './ToolsSection';
 import ShareButtons from './ShareButtons';
 
 const Abajora = () => {
-  // Temporary placeholder values for the ShareButtons component
   const title = "Abajora Project";
-  const url = "https://placeholder.com/abajora";
-  const imageUrl = "https://placeholder.com/path-to-image.jpg";
+  const url = "https://yourwebsite.com/abajora";
+  const imageUrl = "https://yourwebsite.com/path-to-image.jpg";
 
   return (
     <>
