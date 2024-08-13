b79fb50d084da684bca952814b16c47a48d60b08
Galah-Studio <hamid@galah.tv>
2024-08-13 03:11:41 +0300

    Update Abajora.tsx

--- components/projects/Abajora.tsx
+++ components/projects/Abajora.tsx
@@ -3,12 +3,14 @@ import SpotLight from './SpotLight';
 import StorySection from './StorySection';
 import CraftSection from './CraftSection';
 import ToolsSection from './ToolsSection';
+import ShareButtons from './ShareButtons';
 
 const Abajora = () => {
   return (
     <>
       <ProjectHeader />
       <SpotLight />
+      <ShareButtons />
       <StorySection />
       <CraftSection />
       <ToolsSection />
