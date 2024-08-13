f02f506914f0b891d062a2e47ceb32d316410b30
Galah-Studio <hamid@galah.tv>
2024-08-13 05:21:38 +0300

    Create index.tsx

---
+++ components/projects/Abajora/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ProjectHeader from './ProjectHeader';
+import SpotLight from './SpotLight';
+import StorySection from './StorySection';
+import CraftSection from './CraftSection';
+import ToolsSection from './ToolsSection';
+import ShareButtons from './ShareButtons';
+
+const Abajora = () => {
+  const title = "Abajora Project";
+  const url = "https://final-tau-rose.vercel.app/projects/abajora";
+  const imageUrl = "https://yourwebsite.com/path-to-image.jpg";
+
+  return (
+    <>
+      <ProjectHeader />
+      <SpotLight />
+      <StorySection />
+      <CraftSection />
+      <ToolsSection />
+      <ShareButtons title={title} url={url} imageUrl={imageUrl} />
+    </>
+  );
+};
+
+export default Abajora;
