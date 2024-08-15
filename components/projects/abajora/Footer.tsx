ecdda588022179b98e42ed1639f5be410c7ecc8f
Galah-Studio <hamid@galah.tv>
2024-08-15 23:41:09 +0300

    Create Footer.tsx

---
+++ components/projects/abajora/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-backbanner-dark text-white py-8">
+      <div className="max-w-6xl mx-auto text-center">
+        <p className="font-maven">&copy; 2024 Galah. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
