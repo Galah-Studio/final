60de886fd81b4de929ba5164291ab249d4b105b6
Galah-Studio <hamid@galah.tv>
2024-08-15 22:20:06 +0300

    Update ProjectHeader.tsx

--- components/projects/abajora/ProjectHeader.tsx
+++ components/projects/abajora/ProjectHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import { useActiveSectionContext } from '../../../context/active-section-context'; // Adjusted the import path
+import { useActiveSectionContext } from '../../../context/active-section-context';
 
 const ProjectHeader = () => {
   const { activeSection } = useActiveSectionContext();
