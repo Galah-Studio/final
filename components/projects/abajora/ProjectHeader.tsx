07f6daea981d5a89021206f881951d23d83bb3f6
Galah-Studio <hamid@galah.tv>
2024-08-15 23:35:56 +0300

    Update ProjectHeader.tsx

--- components/projects/abajora/ProjectHeader.tsx
+++ components/projects/abajora/ProjectHeader.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
-import { useActiveSectionContext } from '../../context/active-section-context';
+import { useActiveSectionContext } from '../../../context/active-section-context';
 
 const ProjectHeader = () => {
   const { activeSection } = useActiveSectionContext();
 
   return (
-    <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full z-50 bg-dark-900 shadow-lg">
-      <nav className="flex justify-between items-center max-w-6xl mx-auto py-4 px-6 text-white">
-        <div className="text-xl font-bold">Galah Studio</div>
-        <ul className="flex space-x-8 text-gray-300">
+    <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full z-50 bg-backbanner-dark shadow-lg">
+      <nav className="flex justify-between items-center max-w-6xl mx-auto py-4 px-6">
+        <div className="text-xl font-bold text-galah">Galah Studio</div>
+        <ul className="flex space-x-8 text-white">
           <li>
             <Link href="#spotlight">
-              <a className={activeSection === 'SpotLight' ? 'text-pink-500' : ''}>Spot Light</a>
+              <a className={activeSection === 'SpotLight' ? 'text-selectedbanner-dark' : ''}>Spot Light</a>
             </Link>
           </li>
           <li>
             <Link href="#story">
-              <a className={activeSection === 'Story' ? 'text-pink-500' : ''}>Story</a>
+              <a className={activeSection === 'Story' ? 'text-selectedbanner-dark' : ''}>Story</a>
             </Link>
           </li>
           <li>
             <Link href="#craft">
-              <a className={activeSection === 'Craft' ? 'text-pink-500' : ''}>Craft</a>
+              <a className={activeSection === 'Craft' ? 'text-selectedbanner-dark' : ''}>Craft</a>
             </Link>
           </li>
           <li>
             <Link href="#tools">
-              <a className={activeSection === 'Tools' ? 'text-pink-500' : ''}>Tools</a>
+              <a className={activeSection === 'Tools' ? 'text-selectedbanner-dark' : ''}>Tools</a>
             </Link>
           </li>
         </ul>
