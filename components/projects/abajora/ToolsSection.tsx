3a5959924f345cbb637ca13fc07765e05277b253
Galah-Studio <hamid@galah.tv>
2024-08-15 23:24:05 +0300

    Update ToolsSection.tsx

--- components/projects/abajora/ToolsSection.tsx
+++ components/projects/abajora/ToolsSection.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import SectionHeading from '../../section-heading';
 
 const ToolsSection = () => {
   return (
-    <section id="tools" className="max-w-4xl mx-auto py-16 px-6">
-      <h2 className="text-3xl font-bold mb-4">Tools Used</h2>
-      <p className="text-gray-700 leading-relaxed">
-        Blender <br />
-        After Effects <br />
-        Photoshop
-      </p>
+    <section id="tools" className="py-16 bg-dark-800 text-white">
+      <div className="max-w-5xl mx-auto">
+        <SectionHeading title="Tools Used" />
+        <div className="flex flex-wrap mt-6">
+          <span className="mr-4 mb-4 text-lg">Blender</span>
+          <span className="mr-4 mb-4 text-lg">After Effects</span>
+          <span className="mr-4 mb-4 text-lg">Photoshop</span>
+          {/* Add more tools as necessary */}
+        </div>
+      </div>
     </section>
   );
 };
