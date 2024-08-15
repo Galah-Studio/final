e4d978f3e38b1b1150cc1ad36632a82571e9fc12
Galah-Studio <hamid@galah.tv>
2024-08-15 23:24:42 +0300

    Update ShareButtons.tsx

--- components/projects/abajora/ShareButtons.tsx
+++ components/projects/abajora/ShareButtons.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
 
-// Define the type for the props
-type ShareButtonsProps = {
-  title: string;
-  url: string;
-  imageUrl: string;
-};
-
-const ShareButtons: React.FC<ShareButtonsProps> = ({ title, url, imageUrl }) => {
+const ShareButtons = ({ title, url, imageUrl }) => {
   return (
-    <div className="flex space-x-4">
-      <a
-        href={`https://twitter.com/share?url=${url}&text=${title}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src="/twitter-icon.svg" alt="Share on Twitter" />
+    <div className="flex space-x-4 mt-8">
+      <a href={`https://twitter.com/share?url=${url}&text=${title}`} target="_blank" rel="noopener noreferrer">
+        <img src="/icons/twitter-icon.svg" alt="Share on Twitter" className="w-8 h-8" />
       </a>
-      <a
-        href={`https://www.instagram.com/?url=${url}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src="/instagram-icon.svg" alt="Share on Instagram" />
+      <a href={`https://www.instagram.com/?url=${url}`} target="_blank" rel="noopener noreferrer">
+        <img src="/icons/instagram-icon.svg" alt="Share on Instagram" className="w-8 h-8" />
       </a>
     </div>
   );
