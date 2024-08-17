74daaae02c3045cd9873495c95f1104c7ea2340c
Galah-Studio <hamid@galah.tv>
2024-08-17 15:47:37 +0300

    Update abajora.tsx

--- pages/projects/abajora.tsx
+++ pages/projects/abajora.tsx
@@ -7,7 +7,7 @@ import ToolsSection from '../../components/projects/abajora/ToolsSection';
 import ShareButtons from '../../components/projects/abajora/ShareButtons';
 import Footer from '../../components/footer';
 // Import the context provider and test export
-import { ActiveSectionContextProvider, testExport } from '../../context/active-section-context';
+import { useActiveSectionContext, testExport } from '../../context/active-section-context';
 
 console.log(testExport); // Temporary debug statement to ensure import works
 
