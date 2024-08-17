54385a10e2ce178f49346b793e1d6dd4a96048ba
Galah-Studio <hamid@galah.tv>
2024-08-18 00:24:45 +0300

    Update abajora.tsx

--- pages/projects/abajora.tsx
+++ pages/projects/abajora.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ProjectHeader from '../components/projects/abajora/ProjectHeader';
-import SpotLight from '../components/projects/abajora/SpotLight';
-import CraftSection from '../components/projects/abajora/CraftSection';
-import StorySection from '../components/projects/abajora/StorySection';
-import ToolsSection from '../components/projects/abajora/ToolsSection';
-import ShareButtons from '../components/projects/abajora/ShareButtons';
+import ProjectHeader from '../../components/projects/abajora/ProjectHeader';
+import SpotLight from '../../components/projects/abajora/SpotLight';
+import CraftSection from '../../components/projects/abajora/CraftSection';
+import StorySection from '../../components/projects/abajora/StorySection';
+import ToolsSection from '../../components/projects/abajora/ToolsSection';
+import ShareButtons from '../../components/projects/abajora/ShareButtons';
 import Footer from '../components/Footer';
 
 const abajora = () => {
