1310dc707fd91e7bb9b19a513ac4bed64f2f4933
Galah-Studio <hamid@galah.tv>
2024-08-18 00:02:51 +0300

    Update abajora.tsx

--- pages/projects/abajora.tsx
+++ pages/projects/abajora.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
-import ProjectHeader from '../../components/projects/abajora/ProjectHeader';
-import SpotLight from '../../components/projects/abajora/SpotLight';
-import StorySection from '../../components/projects/abajora/StorySection';
-import CraftSection from '../../components/projects/abajora/CraftSection';
-import ToolsSection from '../../components/projects/abajora/ToolsSection';
-import ShareButtons from '../../components/projects/abajora/ShareButtons';
-import Footer from '../../components/footer';
-// Import the context provider and test export
-import { useActiveSectionContext, testExport } from '../../context/active-section-context';
-
-console.log(testExport); // Temporary debug statement to ensure import works
+import ProjectHeader from '../components/projects/Abajora/ProjectHeader';
+import SpotLight from '../components/projects/Abajora/SpotLight';
+import CraftSection from '../components/projects/Abajora/CraftSection';
+import StorySection from '../components/projects/Abajora/StorySection';
+import ToolsSection from '../components/projects/Abajora/ToolsSection';
+import ShareButtons from '../components/projects/Abajora/ShareButtons';
+import Footer from '../components/Footer';
 
 const Abajora = () => {
   return (
-    <useActiveSectionContext>
-      <div className="bg-custom-dark text-white">
-        <ProjectHeader />
-        <SpotLight />
-        <StorySection />
-        <CraftSection />
-        <ToolsSection />
-        <div className="flex justify-center">
-          <ShareButtons title="Abajora Project" url="http://yourwebsite.com/abajora" imageUrl="path/to/image.jpg" />
-        </div>
-        <Footer />
-      </div>
-    </useActiveSectionContext>
+    <div className="bg-custom-dark text-white">
+      <ProjectHeader />
+      <SpotLight />
+      <StorySection />
+      <CraftSection />
+      <ToolsSection />
+      <ShareButtons />
+      <Footer />
+    </div>
   );
 };
 
