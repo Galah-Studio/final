4cc84fb80450c8dbce26a995d22d65392cc07181
Galah-Studio <hamid@galah.tv>
2024-08-22 18:41:30 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,15 +19,15 @@ export const viewport: Viewport = {
 };
 
 // site metadata
-export const metadata: Metadata = {
-  title: `${OWNER_NAME.split(" ")[0]} | Personal Portfolio`,
-  description: `${
-    OWNER_NAME.split(" ")[0]
-  } is a full-stack developer with 4 years of experience.`,
-  authors: {
-    name: OWNER_NAME,
-    url: EXTRA_LINKS.github,
-  },
+//export const metadata: Metadata = {
+  //title: `${OWNER_NAME.split(" ")[0]} | Personal Portfolio`,
+  //description: `${
+    //OWNER_NAME.split(" ")[0]
+  //} is a full-stack developer with 4 years of experience.`,
+  //authors: {
+    //name: OWNER_NAME,
+    //url: EXTRA_LINKS.github,
+  //},
   keywords: [
     "reactjs",
     "nextjs",
