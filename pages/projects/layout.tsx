983cfce61fbe471743414ae64e0bc11bf01973e8
Galah-Studio <hamid@galah.tv>
2024-08-22 19:14:20 +0300

    Update layout.tsx

--- pages/projects/layout.tsx
+++ pages/projects/layout.tsx
@@ -1,6 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 
@@ -14,16 +14,10 @@ import "./globals.css";
 // inter font export
 const inter = Inter({ subsets: ["latin"] });
 
-export const viewport: Viewport = {
-  themeColor: "#CCD6E0",
-};
-
-// site metadata
+// Site metadata (Server-side code)
 export const metadata: Metadata = {
   title: `${OWNER_NAME.split(" ")[0]} | Personal Portfolio`,
-  description: `${
-    OWNER_NAME.split(" ")[0]
-  } is a full-stack developer with 4 years of experience.`,
+  description: `${OWNER_NAME.split(" ")[0]} is a full-stack developer with 4 years of experience.`,
   authors: {
     name: OWNER_NAME,
     url: EXTRA_LINKS.github,
@@ -51,37 +45,31 @@ export const metadata: Metadata = {
   ],
   manifest: "/manifest.json",
   other: {
-    "google-site-verification":
-      process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION_KEY!,
+    "google-site-verification": process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION_KEY!,
   },
 };
 
-// root layout
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// Root layout
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="!scroll-smooth">
       <body
         className={`${inter.className} bg-gray-50 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 text-gray-950 relative pt-28 sm:pt-36`}
       >
-        {/* bg blur effect */}
+        {/* Background blur effect */}
         <div className="bg-[#fbe2e3] dark:bg-[#946263] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
         <div className="bg-[#dbd7fb] dark:bg-[#676394] absolute top-[-1rem] -z-10 left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
 
-        {/* theme context provider */}
+        {/* Theme and Active Section Context Providers */}
         <ThemeContextProvider>
-          {/* active section context provider */}
           <ActiveSectionContextProvider>
-            {/* header */}
+            {/* Header */}
             <Header />
             {children}
-            {/* footer */}
+            {/* Footer */}
             <Footer />
 
-            {/* toaster */}
+            {/* Toaster Notifications */}
             <aside>
               <Toaster
                 position="top-right"
@@ -91,17 +79,17 @@ export default function RootLayout({
               />
             </aside>
 
-            {/* theme switcher */}
+            {/* Theme Switch */}
             <aside>
               <ThemeSwitch />
             </aside>
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
 
-        {/* vercel analytics */}
+        {/* Vercel Analytics */}
         <Analytics />
 
-        {/* vercel speed insights */}
+        {/* Vercel Speed Insights */}
         <SpeedInsights />
       </body>
     </html>
