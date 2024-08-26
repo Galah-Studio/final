363b14b258a3bd16da4cb2924e6b7037b19dd71e
Galah-Studio <hamid@galah.tv>
2024-08-26 23:34:06 +0300

    Update abajora.tsx

--- pages/projects/abajora.tsx
+++ pages/projects/abajora.tsx
@@ -1,38 +1,11 @@
-// Importing necessary components from the specified location ("@/components").
-import {
-  About,
-  Contact,
-  Experience,
-  Intro,
-  Projects,
-  SectionDivider,
-  Skills,
-} from "@/components";
+// Minimal example: import only a basic component to ensure rendering.
+import { Intro } from "@/components";
 
-// This is the main component for the Abajora project page
 export default function Abajora() {
   return (
     <main className="flex flex-col items-center px-4">
-      {/* The Intro component is displayed at the top of the page */}
       <Intro />
-
-      {/* A Vertical line or divider separating sections on the page */}
-      <SectionDivider />
-
-      {/* The About component provides information about the author or user */}
-      <About />
-
-      {/* The Projects component displays information about projects */}
-      <Projects />
-
-      {/* The Skills component shows the skills of the author or user */}
-      <Skills />
-
-      {/* The Experience component displays work or education experience */}
-      <Experience />
-
-      {/* The Contact component allows users to get in touch */}
-      <Contact />
+      {/* Add other components one by one here to test */}
     </main>
   );
 }
