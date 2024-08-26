ea7750dd6f4a5f42cb12adca4e5c0b630ed152d1
Galah-Studio <hamid@galah.tv>
2024-08-27 01:37:52 +0300

    Update abajora.tsx

--- pages/projects/abajora.tsx
+++ pages/projects/abajora.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Intro } from "@/components"; // Import the Intro component
 
 // Simple Header Component
 function Header() {
@@ -10,6 +11,7 @@ export default function Abajora() {
   return (
     <main style={{ minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", backgroundColor: "white" }}>
       <Header />
+      <Intro /> {/* Reintroduce the Intro component */}
       <div style={{ width: "200px", height: "200px", backgroundColor: "red", borderRadius: "50%" }}></div>
     </main>
   );
