6eea4773df279aa9cb5aabe2c992342d77282790
Galah-Studio <hamid@galah.tv>
2024-08-27 01:36:00 +0300

    Update abajora.tsx

--- pages/projects/abajora.tsx
+++ pages/projects/abajora.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-// This is the main component for the Abajora project page
+// Simple Header Component
+function Header() {
+  return <h1 style={{ color: "black", fontSize: "2rem" }}>Hello, Abajora!</h1>;
+}
+
+// Main Abajora Component
 export default function Abajora() {
   return (
-    <main style={{ minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
-      {/* Drawing a big red circle */}
+    <main style={{ minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", backgroundColor: "white" }}>
+      <Header />
       <div style={{ width: "200px", height: "200px", backgroundColor: "red", borderRadius: "50%" }}></div>
     </main>
   );
