6c446a51fd3067ceceea8169b3ffbe225bded294
Galah-Studio <hamid@galah.tv>
2024-08-27 06:00:57 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/projects/abajora/**/*.{js,ts,jsx,tsx}",
+  "./pages/projects/**/*.{js,ts,jsx,tsx}",
+],
+
   theme: {
     extend: {
       fontFamily: {
