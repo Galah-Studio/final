f01fcdb148bb742d196bc2d32c33410b98099996
Galah-Studio <hamid@galah.tv>
2024-08-30 10:31:36 +0300

    Update page.tsx

--- app/abajora/page.tsx
+++ app/abajora/page.tsx
@@ -1,30 +1,33 @@
 import {
-  abajoraProjectHeader,
-  abajoraSpotLight,
-  abajoraStorySection,
-  abajoraCraftSection,
-  abajoraToolsSection,
-  abajoraShareButtons,
+  abajoraProjectHeader as ProjectHeader,
+  abajoraSpotLight as SpotLight,
+  abajoraStorySection as StorySection,
+  abajoraCraftSection as CraftSection,
+  abajoraToolsSection as ToolsSection,
+  abajoraShareButtons as ShareButtons,
 } from "@/components";
 
-
+// This is the main component for the Abajora project page.
+export default function Abajora() {
   return (
     <main className="flex flex-col items-center px-4 bg-custom-dark text-white">
       {/* The ProjectHeader component displays the header section of the project */}
-      <abajoraProjectHeader />
+      <ProjectHeader />
 
       {/* The SpotLight component highlights a key visual of the project */}
-      <abajoraSpotLight />
+      <SpotLight />
 
       {/* The StorySection component provides the narrative of the project */}
-      <abajoraStorySection />
+      <StorySection />
 
       {/* The CraftSection component details the process behind the project */}
-      <abajoraToolsSection />
+      <CraftSection />
 
       {/* The ToolsSection component lists the tools and technologies used */}
-      <abajoraShareButtons />
+      <ToolsSection />
 
+      {/* The ShareButtons component allows users to share the project on social media */}
+      <ShareButtons />
     </main>
   );
 }
