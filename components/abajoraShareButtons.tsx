ed2cfef91554a4e376f81e268ede1b66c56fb07d
Galah-Studio <hamid@galah.tv>
2024-08-30 16:44:39 +0300

    Update abajoraShareButtons.tsx

--- components/abajoraShareButtons.tsx
+++ components/abajoraShareButtons.tsx
@@ -1,19 +1,23 @@
 // components/abajora_sharebuttons.tsx
 
+import { useThemeContext } from "@/context/theme-context";
+
 interface ShareButtonsProps {
   title: string;
   url: string;
   imageUrl: string;
 }
 
-const AbajoraShareButtons: React.FC<ShareButtonsProps> = ({ title, url, imageUrl }) => {
+const abajoraShareButtons: React.FC<ShareButtonsProps> = ({ title, url, imageUrl }) => {
+  const { theme } = useThemeContext();
+
   return (
     <div className="flex space-x-4 mt-4">
       <a
         href={`https://twitter.com/share?url=${url}&text=${title}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="text-white"
+        className={`${theme === "light" ? "text-black" : "text-white"}`}
       >
         <img src="/path/to/twitter-icon.svg" alt="Twitter" className="w-8 h-8" />
       </a>
@@ -21,7 +25,7 @@ const AbajoraShareButtons: React.FC<ShareButtonsProps> = ({ title, url, imageUrl
         href={`https://www.instagram.com/?url=${url}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="text-white"
+        className={`${theme === "light" ? "text-black" : "text-white"}`}
       >
         <img src="/path/to/instagram-icon.svg" alt="Instagram" className="w-8 h-8" />
       </a>
