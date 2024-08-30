4d331d3499d9b8f979f11d30fc6832de03c0e954
Galah-Studio <hamid@galah.tv>
2024-08-31 01:40:42 +0300

    Update abajoraSpotLight.tsx

--- components/abajoraSpotLight.tsx
+++ components/abajoraSpotLight.tsx
@@ -4,7 +4,7 @@ const AbajoraSpotLight = () => {
   return (
     <section className="bg-custom-dark text-white p-8 min-h-screen flex items-center justify-center">
       <div className="max-w-4xl text-center">
-        <img src="/path/to/your/image.jpg" alt="Spotlight Image" className="mx-auto mb-8" />
+        <img src="/vivi_full.png" alt="Spotlight Image" className="mx-auto mb-8" />
         <h1 className="font-comfortaa text-4xl mb-4">The Title Goes Here</h1>
       </div>
     </section>
