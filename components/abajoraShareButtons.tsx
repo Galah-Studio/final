57cef3dd2803f6b8ce146de521c6c319bbc562e1
Galah-Studio <hamid@galah.tv>
2024-08-31 00:50:04 +0300

    Update abajoraShareButtons.tsx

--- components/abajoraShareButtons.tsx
+++ components/abajoraShareButtons.tsx
@@ -1,5 +1,6 @@
 // components/abajora_sharebuttons.tsx
 
+import { FaInstagram, FaXTwitter } from "react-icons/fa6";
 import { useThemeContext } from "@/context/theme-context";
 
 interface ShareButtonsProps {
@@ -12,22 +13,22 @@ const abajoraShareButtons: React.FC<ShareButtonsProps> = ({ title, url, imageUrl
   const { theme } = useThemeContext();
 
   return (
-    <div className="flex space-x-4 mt-4">
+    <div className="fixed right-4 bottom-4 flex flex-col space-y-4">
       <a
         href={`https://twitter.com/share?url=${url}&text=${title}`}
         target="_blank"
         rel="noopener noreferrer"
-        className={`${theme === "light" ? "text-black" : "text-white"}`}
+        className={`p-4 rounded-full ${theme === "light" ? "bg-white text-black" : "bg-black text-white"}`}
       >
-        <img src="/path/to/twitter-icon.svg" alt="Twitter" className="w-8 h-8" />
+        <FaXTwitter className="w-8 h-8" />
       </a>
       <a
         href={`https://www.instagram.com/?url=${url}`}
         target="_blank"
         rel="noopener noreferrer"
-        className={`${theme === "light" ? "text-black" : "text-white"}`}
+        className={`p-4 rounded-full ${theme === "light" ? "bg-white text-black" : "bg-black text-white"}`}
       >
-        <img src="/path/to/instagram-icon.svg" alt="Instagram" className="w-8 h-8" />
+        <FaInstagram className="w-8 h-8" />
       </a>
     </div>
   );
