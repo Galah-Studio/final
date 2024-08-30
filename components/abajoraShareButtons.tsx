b2583901dfe09e276a070413905b7e5fec057cd0
Galah-Studio <hamid@galah.tv>
2024-08-31 01:37:45 +0300

    Update abajoraShareButtons.tsx

--- components/abajoraShareButtons.tsx
+++ components/abajoraShareButtons.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram, FaXTwitter } from "react-icons/fa6";
 import { useThemeContext } from "@/context/theme-context";
 
 interface ShareButtonsProps {
@@ -10,28 +9,60 @@ interface ShareButtonsProps {
   imageUrl: string;
 }
 
-const abajoraShareButtons: React.FC<ShareButtonsProps> = ({ title, url, imageUrl }) => {
+const abajoraShareButtons: React.FC<ShareButtonsProps> = ({
+  title,
+  url,
+  imageUrl,
+}) => {
   const { theme } = useThemeContext();
 
   return (
-    <div className="flex justify-center mt-4">
+    <div className="flex justify-center gap-4 mt-6">
       <a
         href={`https://twitter.com/share?url=${url}&text=${title}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="p-3 rounded-full bg-gray-700 dark:bg-gray-800 flex items-center justify-center mx-2"
-        style={{ width: '40px', height: '40px' }}
+        aria-label="Share on Twitter"
+        className={`p-4 text-gray-700 flex items-center justify-center rounded-full outline-none transition ${
+          theme === "light"
+            ? "bg-white hover:bg-gray-100"
+            : "bg-white/10 text-white/60 hover:text-white hover:bg-white/20"
+        }`}
+        style={{
+          backgroundColor: "#ff2b69",
+          width: "50px",
+          height: "50px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "50%",
+          color: "white",
+        }}
       >
-        <FaXTwitter className="text-white" />
+        <FaXTwitter size={24} />
       </a>
       <a
         href={`https://www.instagram.com/?url=${url}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="p-3 rounded-full bg-gray-700 dark:bg-gray-800 flex items-center justify-center mx-2"
-        style={{ width: '40px', height: '40px' }}
+        aria-label="Share on Instagram"
+        className={`p-4 text-gray-700 flex items-center justify-center rounded-full outline-none transition ${
+          theme === "light"
+            ? "bg-white hover:bg-gray-100"
+            : "bg-white/10 text-white/60 hover:text-white hover:bg-white/20"
+        }`}
+        style={{
+          backgroundColor: "#ff2b69",
+          width: "50px",
+          height: "50px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "50%",
+          color: "white",
+        }}
       >
-        <FaInstagram className="text-white" />
+        <FaInstagram size={24} />
       </a>
     </div>
   );
