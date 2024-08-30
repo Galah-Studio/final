c1aa40b42b4e4219b9d6da5b2126ace01d7cb8f0
Galah-Studio <hamid@galah.tv>
2024-08-30 11:20:11 +0300

    Update page.tsx

--- app/abajora/page.tsx
+++ app/abajora/page.tsx
@@ -1,11 +1,11 @@
-import { abajoraProjectHeader } from "@/components";
+import { abajoraProjectHeader as AbajoraProjectHeader } from "@/components";
 
 // This is the main component for the Abajora project page
 export default function AbajoraPage() {
   return (
     <main className="flex flex-col items-center px-4 bg-custom-dark text-white">
       {/* The ProjectHeader component displays the header section of the project */}
-      <abajoraProjectHeader />
+      <AbajoraProjectHeader />
     </main>
   );
 }
