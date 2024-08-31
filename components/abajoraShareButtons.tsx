9d68c86667f30b5ee6b264a785555ca07373c2db
Galah-Studio <hamid@galah.tv>
2024-08-31 03:03:11 +0300

    Update abajoraShareButtons.tsx

--- components/abajoraShareButtons.tsx
+++ components/abajoraShareButtons.tsx
@@ -2,6 +2,7 @@
 
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { useThemeContext } from "@/context/theme-context";
 
 interface ShareButtonsProps {
   title: string;
@@ -9,28 +10,28 @@ interface ShareButtonsProps {
   imageUrl: string;
 }
 
-const AbajoraShareButtons: React.FC<ShareButtonsProps> = ({
-  title,
-  url,
-  imageUrl,
-}) => {
+const AbajoraShareButtons: React.FC<ShareButtonsProps> = ({ title, url }) => {
+  const { theme } = useThemeContext();
+
   return (
-    <div className="fixed right-4 bottom-1/2 transform translate-y-1/2 z-30 flex flex-col space-y-4">
+    <div className="flex flex-col space-y-4 fixed right-8 top-[50%] transform -translate-y-1/2 z-10">
       <a
         href={`https://twitter.com/share?url=${url}&text=${title}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="p-3 bg-gray-900 text-white rounded-full hover:bg-pink-500 transition"
+        className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-white transition transform hover:scale-105"
+        aria-label="Share on Twitter"
       >
-        <FaXTwitter className="w-6 h-6" />
+        <FaXTwitter size={24} />
       </a>
       <a
         href={`https://www.instagram.com/?url=${url}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="p-3 bg-gray-900 text-white rounded-full hover:bg-pink-500 transition"
+        className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-white transition transform hover:scale-105"
+        aria-label="Share on Instagram"
       >
-        <FaInstagram className="w-6 h-6" />
+        <FaInstagram size={24} />
       </a>
     </div>
   );
