5599f2023f5c9f6608fdbbed7e49bbea13ca53c0
Galah-Studio <hamid@galah.tv>
2024-08-31 07:12:53 +0300

    Update page.tsx

--- app/abajora/page.tsx
+++ app/abajora/page.tsx
@@ -1,6 +1,8 @@
-"use client"; // Ensure this runs on the client-side
+// app/abajora/page.tsx
 
-import AbajoraProjectHeader from "@/components/abajoraProjectHeader";
+"use client";
+
+import AbajoraLogos from "@/components/abajoraLogos";
 import AbajoraSpotLight from "@/components/abajoraSpotLight";
 import AbajoraStorySection from "@/components/abajoraStorySection";
 import AbajoraCraftSection from "@/components/abajoraCraftSection";
@@ -8,40 +10,49 @@ import AbajoraToolsSection from "@/components/abajoraToolsSection";
 import AbajoraShareButtons from "@/components/abajoraShareButtons";
 
 export default function Page() {
-  // Define the props to pass to the ShareButtons component
-  const shareProps = {
-    title: "Abajora Project",
-    url: "https://Galah.tv/abajora",
-    imageUrl: "https://Galah.tv/_next/image?url=%2Fvivi_full.png",
-  };
-
   return (
-    <main className="bg-custom-dark text-white">
-      {/* Project Header Section */}
-      <AbajoraProjectHeader />
+    <main className="bg-custom-dark text-white relative">
+      <div className="absolute inset-0">
+        <img
+          src="/vivi_full.png"
+          alt="Background Image"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+      </div>
 
-      {/* Spotlight Section */}
-      <section id="spotlight">
+      {/* Logos Section */}
+      <div className="relative z-10 flex flex-col items-center justify-center h-screen">
+        <AbajoraLogos />
+        <h1 className="text-white text-center mt-8 text-3xl md:text-5xl font-bold">
+          The Title Goes Here
+        </h1>
+      </div>
+
+      {/* Share Buttons */}
+      <div className="relative z-10">
         <AbajoraSpotLight />
-      </section>
-
-      {/* Share Buttons Section */}
-      <AbajoraShareButtons {...shareProps} />
+      </div>
 
       {/* Story Section */}
-      <section id="story">
+      <section className="relative z-10" id="story">
         <AbajoraStorySection />
       </section>
 
       {/* Craft Section */}
-      <section id="craft">
+      <section className="relative z-10" id="craft">
         <AbajoraCraftSection />
       </section>
 
       {/* Tools Section */}
-      <section id="tools">
+      <section className="relative z-10" id="tools">
         <AbajoraToolsSection />
       </section>
+
+      {/* Share Buttons Fixed */}
+      <div className="fixed top-1/2 right-4 transform -translate-y-1/2 z-20">
+        <AbajoraShareButtons />
+      </div>
     </main>
   );
 }
