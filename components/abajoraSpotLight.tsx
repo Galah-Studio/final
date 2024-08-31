b93059847bf4943d13a0211c257dcfc2b12dd683
Galah-Studio <hamid@galah.tv>
2024-08-31 16:02:42 +0300

    Update abajoraSpotLight.tsx

--- components/abajoraSpotLight.tsx
+++ components/abajoraSpotLight.tsx
@@ -1,17 +1,38 @@
-// components/abajora_spotlight.tsx
+// components/abajoraSpotLight.tsx
 
-import React from 'react';
-import { FaInstagram } from 'react-icons/fa';
-import { FaXTwitter } from 'react-icons/fa6';
+import Image from 'next/image';
+import { FaInstagram, FaXTwitter } from 'react-icons/fa6';
+import GalahLogo from '@/public/galah_logo.svg'; // Adjust the path to your logo file
 
 const AbajoraSpotLight = () => {
   return (
-    <div className="relative">
-      <div className="fixed top-1/2 right-4 flex flex-col items-center space-y-4 transform -translate-y-1/2">
-        <FaXTwitter className="text-white text-3xl hover:text-pink-500 transition-colors duration-300" />
-        <FaInstagram className="text-white text-3xl hover:text-pink-500 transition-colors duration-300" />
+    <section className="relative w-full h-screen overflow-hidden">
+      {/* Spotlight Image */}
+      <div className="relative w-full h-full">
+        <Image
+          src="/vivi_full.png" // Ensure this path is correct
+          alt="Spotlight"
+          layout="fill"
+          objectFit="cover"
+          className="z-0"
+          style={{
+            background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0.8) 100%)',
+          }}
+        />
+        
+        {/* Logos */}
+        <div className="absolute inset-0 flex flex-col justify-center items-center">
+          <Image src={GalahLogo} alt="Galah Logo" width={100} height={100} className="mb-4" />
+          <Image src={GalahLogo} alt="Abajora Logo" width={100} height={100} />
+        </div>
+
+        {/* Title */}
+        <h1 className="absolute bottom-8 w-full text-center text-4xl font-bold text-white">
+          The Title Goes Here
+        </h1>
       </div>
-    </div>
+
+    </section>
   );
 };
 
