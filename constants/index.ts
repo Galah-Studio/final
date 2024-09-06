69ed8a7693920d16c2fd6cc014e3ab5dfc07d3ae
Galah-Studio <hamid@galah.tv>
2024-09-06 10:49:45 +0300

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -81,10 +81,6 @@ export const MAIN_PAGE_LINKS = [
 // Navigation links for the Abajora page
 export const ABAJORA_PAGE_LINKS = [
   {
-    name: "Home",
-    hash: "#home",
-  },
-  {
     name: "SpotLight",
     hash: "#spotlight",
   },
