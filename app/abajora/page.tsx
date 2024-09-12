2672e3efc5f7bcdad302fe9f3510397ad3780d44
Galah-Studio <hamid@galah.tv>
2024-09-12 19:05:42 +0300

    Update page.tsx

--- app/abajora/page.tsx
+++ app/abajora/page.tsx
@@ -7,7 +7,7 @@ import AbajoraCraftSection from "@/components/abajoraCraftSection";
 import AbajoraToolsSection from "@/components/abajoraToolsSection";
 import AbajoraShareButtons from "@/components/abajoraShareButtons";
 import AbajoraLogos from "@/components/abajoraLogos";
-
+import { useSectionInView } from "@/lib/hooks"; // Assuming you're using a hook to track section in view
 
 export default function Page() {
   // Define the props to pass to the ShareButtons component
@@ -17,18 +17,25 @@ export default function Page() {
     imageUrl: "https://Galah.tv/_next/image?url=%2Fvivi_full.png",
   };
 
+  // Use `useSectionInView` for tracking sections
+  const spotlightRef = useSectionInView("SpotLight");
+  const clientsRef = useSectionInView("Clients");
+  const storyRef = useSectionInView("Story");
+  const craftRef = useSectionInView("Craft");
+  const toolsRef = useSectionInView("Tools");
+
   return (
     <main className="bg-custom-dark text-white">
       {/* Project Header Section */}
       <AbajoraProjectHeader />
 
       {/* Spotlight Section */}
-      <section id="spotlight">
+      <section id="spotlight" ref={spotlightRef}>
         <AbajoraSpotLight />
       </section>
 
        {/* AbajoraLogos Section */}
-      <section id="clients">
+      <section id="clients" ref={clientsRef}>
         <AbajoraLogos />
       </section>
 
@@ -36,17 +43,17 @@ export default function Page() {
       <AbajoraShareButtons {...shareProps} />
 
       {/* Story Section */}
-      <section id="story">
+      <section id="story" ref={storyRef}>
         <AbajoraStorySection />
       </section>
 
       {/* Craft Section */}
-      <section id="craft">
+      <section id="craft" ref={craftRef}>
         <AbajoraCraftSection />
       </section>
 
       {/* Tools Section */}
-      <section id="tools">
+      <section id="tools" ref={toolsRef}>
         <AbajoraToolsSection />
       </section>
     </main>
