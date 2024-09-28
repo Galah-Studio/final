c7af8c9828ab171a23cc2207acbac0ca4a5c0085
Galah-Studio <hamid@galah.tv>
2024-09-28 22:04:22 +0300

    Update abajoraShareButtons.tsx

--- components/abajoraShareButtons.tsx
+++ components/abajoraShareButtons.tsx
@@ -21,7 +21,7 @@ const AbajoraShareButtons: React.FC<ShareButtonsProps> = ({
 
   return (
     <div className="w-full flex justify-end mt-8">
-      <div className="flex items-center space-x-6 mr-42">
+      <div className="flex items-center space-x-6 mr-44">
         <a
           href={`https://x.com/share?url=${encodeURIComponent(
             url
